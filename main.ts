bf9c255208c9dce18a457fc06f38ab8dd32a1ac7
Glaceon-471 <glaceonsnr11@gmail.com>
2024-12-25 18:47:01 +0900

    ちょっと変更

--- main.ts
+++ main.ts
@@ -1,6 +1,30 @@
 import { createBot, getBotIdFromToken, startBot, Intents } from "@discordeno/mod.ts";
 // import { BCDiceVersionCommand, BCDiceRoll } from "./commands.ts";
-import { BCDice } from "./test_commands.ts"
+import { CreateSlashApplicationCommand, Bot, Interaction, InteractionResponseTypes } from "@discordeno/mod.ts";
+import { ApplicationCommandOptionTypes, ApplicationCommandTypes } from "@discordeno/types/shared.ts";
+import { ApplicationCommandOptionChoice } from "@discordeno/transformers/applicationCommandOptionChoice.ts"
+
+interface SlashCommand {
+    info: CreateSlashApplicationCommand;
+    response(bot: Bot, interaction: Interaction): Promise<void>;
+};
+
+export const BCDice: SlashCommand = {
+    info: {
+        name: "bcdice",
+        description: "BCDiceのバージョンを表示します。",
+        type: ApplicationCommandTypes.ChatInput
+    },
+    response: async (bot, interaction) => {
+        return await bot.helpers.sendInteractionResponse(interaction.id, interaction.token, {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+                content: `BCDiceバージョン : 出力できないよ....もう....`,
+                flags: 0b01000000
+            }
+        });
+    }
+}
 
 const BotToken: string = Deno.env.get("BOT_TOKEN")!;
 
